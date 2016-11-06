392ef7ee0dea90a3676542f95d52a61e0d53d03e
Ricky Moorhouse <ricky@samespirit.net>
2016-11-06 09:09:19 +0000

    more tests :D

--- tests/test_web.py
+++ tests/test_web.py
@@ -27,11 +27,13 @@ class weblightTest(unittest.TestCase):
         self.assertEqual(response['red'], 30)
         self.assertEqual(response['green'], 140)
         self.assertEqual(response['blue'], 250)
+        self.assertEqual(w.status(), '1')
         # Turn off
         response = w.power_off()
         self.assertEqual(response['red'], 0)
         self.assertEqual(response['green'], 0)
         self.assertEqual(response['blue'], 0)
+        self.assertEqual(w.status(), '0')
         # Turn off
         response = w.power_on()
         self.assertEqual(response['red'], 30)
@@ -45,3 +47,21 @@ class weblightTest(unittest.TestCase):
         self.assertEqual(response['red'], 255)
         self.assertEqual(response['green'], 255)
         self.assertEqual(response['blue'], 255)
+
+    def testHueSatLum(self):
+        w = pilight.web.WebLight('console')
+        w.power_on()
+        response1 = w.hue(0)
+        response2 = w.lum(100)
+        response3 = w.sat(100)
+        self.assertEqual(response1['red']>0, True)
+        self.assertEqual(response1['green'] < response1['red'], True)
+        self.assertEqual(response1['blue'] < response1['red'], True)
+
+        self.assertEqual(response2['red'], 255)
+        self.assertEqual(response2['green'], 255)
+        self.assertEqual(response2['blue'], 255)
+
+        self.assertEqual(response3['red'], 255)
+        self.assertEqual(response3['green'], 255)
+        self.assertEqual(response3['blue'], 255)
